128fe028d05eceeec7d1c1136544ed79af36e35e
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-21 14:56:49 +0530

    closing and filtering users

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { WebSocketServer, WebSocket } from "ws";
 
 const server = new WebSocketServer({ port: 8000 })
-const users: WebSocket[] = []
+let users: WebSocket[] = []
 server.on('connection', (socket) => {
     users.push(socket)
     socket.send("connection success")
     socket.on('message', (e) => {
+        console.log(users.length);
         users.forEach((user) => {
             user.send(e.toString())
+        })
+        socket.on('close', () => {
+            users = users.filter((user) => user != socket)            
         })
 
     })
