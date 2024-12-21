da1b15293cbbeb02ca2dfca5d7857e47138b4016
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-21 14:34:44 +0530

    basic connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const server = new WebSocketServer({ port: 8000 })
-
-server.on('connection',(socket)=>{
+const users: WebSocket[] = []
+server.on('connection', (socket) => {
+    users.push(socket)
     socket.send("connection success")
-    
+    socket.on('message', (e) => {
+        users.forEach((user) => {
+            user.send(e.toString())
+        })
+        // for (let i = 0; i < users.length; i++) {
+        //     let user = users[i]
+        //     user.send(e.toString())
+        // }
+    })
+
 })
