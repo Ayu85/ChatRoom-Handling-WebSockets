a4787910c6ac6d2387bf7957eb8912df97c2bd19
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-21 19:31:38 +0530

    both message and room id feature done

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const server = new WebSocketServer({ port: 8000 })
 interface User {
-    socket: WebSocket;
+    socket: WebSocket
     roomId: string
 }
 let allUsers: User[] = []
@@ -14,27 +14,36 @@ let allUsers: User[] = []
 //     {socket:socket3,roomId:'4545'},
 // ]
 server.on('connection', (socket) => {
-    allUsers.push({ socket, roomId: "sad" })
     socket.on('message', (e) => {
         // we will rec. the msg as a string , so parse that to object to play with it
+        
         const jsonMsg = JSON.parse(e.toString());
+        
         // if user wants to join
         if (jsonMsg.type === 'join') {
             allUsers.push({
                 socket: socket,
                 roomId: jsonMsg.payload.roomId
             })
+            console.log(allUsers);
+            
         }
         // if user wants to send message
         // it means user has already joined a room with roomId..456825
-        if (jsonMsg.type === 'message') {
+        else if (jsonMsg.type === 'message') {
             // to get the current user's room id
-            const currentUserRoomId = allUsers.find((user) => user.socket == socket)?.roomId
+            const currentUserRoomId = allUsers.find((user) => user.socket === socket)?.roomId
+            console.log(currentUserRoomId);
+            
             // now send that message to each user present in this (currentUserRoomID) 
             allUsers.forEach((user) => {
                 if (user.roomId === currentUserRoomId)
-                    user.socket.send(jsonMsg.payload)
+                    user.socket.send(jsonMsg.payload.message)
             })
+        }
+        else {
+            socket.send("wrong msg");
+
         }
     })
 
